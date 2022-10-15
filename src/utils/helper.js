8d84ad4efd5992ea95735260cd89dfc22514d637
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-10-15 16:03:17 +0530

    ore changes

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,11 @@
 export const prettifyId = (id) => {
   return id.slice(0, 2).toUpperCase() + " " + id.slice(2);
 };
+
+export const generateAvatarText = (name) => {
+  return name
+    .split(" ")
+    .map((word) => word[0])
+    .join("")
+    .toUpperCase();
+};
