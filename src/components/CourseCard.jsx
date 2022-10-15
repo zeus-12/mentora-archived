543898d6a01fd2248054bb82e99039715a723399
Nihad <nigad779@gmail.com>
2022-10-15 13:27:49 +0530

    s

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -4,14 +4,14 @@ import Folder from "../UI components/Folder";
 const CourseCard = ({ name, id }) => {
   return (
    <Link href={`/course/${id}`} passHref>
-    // <div className="border-neutral-600 border-[0.3px]">
-//     <div className="border-neutral-600 rounded-md px-4 truncate hover:cursor-pointer border-[0.3px]">
+    {/* // <div className="border-neutral-600 border-[0.3px]"> */}
+{/* //     <div className="border-neutral-600 rounded-md px-4 truncate hover:cursor-pointer border-[0.3px]"> */}
 
       <Folder>
         <p>{name}</p>
         <p>{id}</p>
       </Folder>
-    // </div>
+    {/* // </div> */}
       </Link>
 
   );
