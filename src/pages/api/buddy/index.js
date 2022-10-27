10709aba9de9b924f524f11bf62eb636048b622f
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-10-27 12:16:19 +0530

    part of prev commit

--- src/pages/api/buddy/index.js
+++ src/pages/api/buddy/index.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
     try {
       await dbConnect();
       const buddyDetails = await Buddy.find()
-        .select("buddyType course_id  message money")
+        .select("buddyType course_id message money")
         .lean();
 
       return res.status(200).json({ message: "success", data: buddyDetails });
