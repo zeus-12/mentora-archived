978a78d3193d0eda7124511a93cac6b338fcf327
Nihad <nigad779@gmail.com>
2022-10-27 21:43:34 +0530

    logo bg

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,11 +5,7 @@ const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-<<<<<<< HEAD
       <div className="pt-4 md:px-8 px-4 lg:px-16 xl:px-32 bg-[#080d13] ">
-=======
-      <div className="pt-4 md:px-8 px-4 lg:px-16 xl:px-32 bg-[#080d13]">
->>>>>>> eba42b2f2d62bfdeb7ead65ebd14f4ac686ee31d
         {children}
       </div>
     </div>
