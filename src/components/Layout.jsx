e55270542fac5a526edcbf4e03dd4fe1a2a6792f
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-10-28 16:47:15 +0530

    few layour changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 // to define a general layout
 
 import Link from "next/link";
+import Feedback from "../pages/feedback";
 import Navbar from "./Navbar";
+
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="pt-4 flex-1 md:px-8 px-4 lg:px-16 xl:px-32 bg-[#080d13]">
+      <div className="flex-1 pt-4 md:px-8 px-4 lg:px-16 xl:px-32 bg-[#080d13]">
         {children}
       </div>
       <div className="flex items-center justify-between py-2 px-8 bg-[#080d13] text-gray-400">
@@ -18,12 +20,6 @@ const Layout = ({ children }) => {
         </Link>
       </div>
     </div>
-    // <NotificationContext.Provider
-    //   value={{ message, type, setMessage, setType }}
-    // >
-    //   {/* <Navbar  /> */}
-    //   <Notification message={message} type={type} />
-    // </NotificationContext.Provider>
   );
 };
 export default Layout;
