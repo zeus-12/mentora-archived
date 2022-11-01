4ecbf468a79bf9677747dee22707c22d4cd471e7
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-11-01 21:51:54 +0530

    added cool-sites

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="flex items-center justify-between py-2 px-8 bg-[#080d13] text-gray-400">
       <Link href="/other">
         <p className="text-green-300 hover:cursor-pointer hover:underline underline-offset-4 ">
-          Other Useful Sites
+          Other cool Sites
         </p>
       </Link>
       <p className="text-center">Made with ❤️ as part of TechSoc IITM.</p>
