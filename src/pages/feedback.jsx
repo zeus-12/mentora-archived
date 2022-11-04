f6e991f3c2b2be7830707b1e7b7b419a94dcb115
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-11-04 14:36:43 +0530

    layout issue with feedback

--- src/pages/feedback.jsx
+++ src/pages/feedback.jsx
@@ -58,7 +58,7 @@ const Feedback = () => {
   };
 
   return (
-    <div className="gap-2 flex-1 flex items-center justify-center h-full flex-col">
+    <div className="gap-2 flex-1 flex items-center sm:justify-center h-full flex-col">
       <p className="text-3xl font-bold pt-4">
         Submit <span className="text-green-400 ">Feedback</span>!
       </p>
